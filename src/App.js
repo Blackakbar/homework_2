7b1c50aeff3c34d0b69b6f2a21979c2607e4e451
Blackakbar <akbar04222@gmail.com>
2023-10-14 13:41:03 +0600

    homework_2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Squares extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftColor: 'red',
+      rightColor: 'blue',
+    };
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      leftColor: prevState.rightColor,
+      rightColor: prevState.leftColor,
+    }));
+  }
+
+  render() {
+    return (
+      <div className='centered'>
+        <div className={`square ${this.state.leftColor}-square`} onClick={this.handleClick}></div>
+        <div className={`square ${this.state.rightColor}-square`} onClick={this.handleClick}></div>
+      </div>
+    );
+  }
 }
 
-export default App;
+class Container extends Component {
+  render() {
+    return <Squares />;
+  }
+}
+
+class SwitchSquares extends Component {
+  render() {
+    return (
+      <div className='switcher'>
+        <div className='switcher-container'>
+          <Container />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SwitchSquares;
